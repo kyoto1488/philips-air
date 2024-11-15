8649cf375f3993345791cbe32c874da532fd2583
Oleh Danylov <kyoto1488@gmail.com>
2024-11-15 03:34:01 +0200

    remove dist

--- dist/settings.js
+++
@@ -1,9 +0,0 @@
-/**
- * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
- */
-export const PLATFORM_NAME = 'PhilipsAir';
-/**
- * This must match the name of your plugin as defined the package.json
- */
-export const PLUGIN_NAME = '@kyoto1488/homebridge-philips-air';
-//# sourceMappingURL=settings.js.map
